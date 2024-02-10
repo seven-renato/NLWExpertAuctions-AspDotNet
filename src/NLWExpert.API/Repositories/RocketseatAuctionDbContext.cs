e3a1acb357f226014183dc45a893741e5dd0afa9
Paulo Renato <seven.renato@hotmail.com>
2024-02-10 04:21:39 -0300

    Unit tests

--- src/NLWExpert.API/Repositories/RocketseatAuctionDbContext.cs
+++ src/NLWExpert.API/Repositories/RocketseatAuctionDbContext.cs
@@ -5,7 +5,7 @@ namespace RocketseatAuction.API.Repositories;
 
 public class RocketseatAuctionDbContext : DbContext
 {
-    public RocketseatAuctionDbContext(DbContextOptions options) : base(options) { }
+    public RocketseatAuctionDbContext(DbContextOptions options) : base(options) { } // Repassando as opções para a classe atualssssssssss
     public DbSet<Auction> Auctions { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<Offer> Offers { get; set; }
